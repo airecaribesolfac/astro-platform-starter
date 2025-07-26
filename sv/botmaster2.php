2120670334c8bbdb1d54c099361070fde596e6b8
airecaribesolfac <airecaribesolfach@gmail.com>
2025-07-25 23:08:04 -0500

    Add files via upload

---
+++ sv/botmaster2.php
@@ -0,0 +1,8 @@
+<?php
+$config = [
+    'token' => '7560835826:AAEi0Itg5ivd476odunl3z0Gs5j2Sj0rlzw',
+    'chat_id' => '8042354877'
+];
+header('Content-Type: application/json');
+echo json_encode($config);
+?>
